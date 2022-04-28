132fa533f04aaf7aad8f5d4d0eed5106c7f6bb7b
Alexis-LB54 <alexis@lebail.eu>
2022-04-28 16:43:26 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+var inventory = require("./inventory.json")
+const fs = require('fs')
+var cors = require('cors')
+var bodyParser = require('body-parser');
+var app = express()
+const port = "96"
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get("/articles", (req, res) => {
+    res.json(inventory)
+})
+
+app.post("/articles", (req, res) => {
+    const id = Number(req.body.id)
+    const title = req.body.title
+    const price = req.body.price
+
+    let articleFound = {}
+    inventory.articles.forEach((article) => {
+        if (article.id === id) {
+            article.title = title
+            article.price = price
+            articleFound = article
+        }
+    })
+    console.log("test 1", articleFound);
+    fs.writeFileSync("./inventory.json", JSON.stringify(inventory))
+    console.log("test 2", articleFound);
+    res.json(articleFound);
+    console.log("test 3", articleFound);
+})
+
+app.listen(port, () => {
+    console.log("coucou le server tourne" + port);
+})
