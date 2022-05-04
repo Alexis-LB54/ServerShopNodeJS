f2a26f42eed0a6cd5d715715f1425bbd4bba621f
Alexis-LB54 <alexis@lebail.eu>
2022-05-04 16:26:29 +0200

    ajout de nodemailer mais non opé

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const port = "96"
 const mysql = require('mysql');
 const csvtojson = require('csvtojson');
 const session = require('express-session');
+const nodemailer = require("nodemailer");
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -59,16 +60,20 @@ app.post("/add", (req, res) => {
 })
 
 db.connect(function (err) {
+
     if (err) throw err;
     console.log("Connecté à la base de données MySQL!");
+
     db.query("CREATE TABLE IF NOT EXISTS articles(id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, description TEXT, price INT, currency varchar(255) DEFAULT '€', brand VARCHAR(255))", function (err, result) {
         if (err) throw err;
         console.log("table articles créée ou déjà créée mais connécté quoi qu'il arrive !");
     });
+
     db.query("CREATE TABLE IF NOT EXISTS informations(id INT AUTO_INCREMENT PRIMARY KEY, compagnie_name VARCHAR(255) NOT NULL, number_of_employees INT, turnover INT)", function (err, result) {
         if (err) throw err;
         console.log("table informations créée ou déjà créée mais connécté quoi qu'il arrive !");
     });
+
     db.query("CREATE TABLE IF NOT EXISTS accounts(id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL)", function (err, result) {
         if (err) throw err;
         console.log("table accounts créée ou déjà créée mais connécté quoi qu'il arrive !");
@@ -93,14 +98,14 @@ app.delete("/delete/:id", (req, res) => {
     const id = req.params.id
     console.log("mon id d'article à supprimer:", id)
     let articleFound = {}
-    
+
     const findId = (article) => article.id == id;
-    
+
     let MyIndex = inventory.articles.findIndex(findId);
     console.log(MyIndex);
 
     inventory.articles.splice(MyIndex, 1);
-    
+
     fs.writeFileSync("./inventory.json", JSON.stringify(inventory))
     res.json(articleFound);
 })
@@ -109,17 +114,17 @@ app.delete("/delete/:id", (req, res) => {
 const fileName = "tableau.csv";
 
 // csvtojson().fromFile(fileName).then(source => {
-    
+
 //     // Fetching the data from each row 
 //     // and inserting to the table "tableau"
 //     for (var i = 0; i < source.length; i++) {
 //         var Name = source[i]["Nom"],
 //         Employees = source[i]["Nombre de salarié"],
 //         CA = source[i]["CA (Chiffre d’affaire) en €"]
-  
+
 //         var insertStatement = `INSERT INTO informations(compagnie_name, number_of_employees, turnover) VALUES (?, ?, ?)`;
 //         var items = [Name, Employees, CA];
-  
+
 //         // Inserting data of current row
 //         // into database
 //         db.query(insertStatement, items, 
@@ -137,6 +142,8 @@ const fileName = "tableau.csv";
 
 
 // système de connection
+
+
 app.get("/myuser", (req, res) => {
     var insert2 = `INSERT INTO accounts (username, password, email) VALUES ('alexis', 'alexis', 'alexis@alexis.eu')`;
     db.query(insert2, function (err, results) {
@@ -146,37 +153,76 @@ app.get("/myuser", (req, res) => {
 })
 
 app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
 }));
 
-app.post("/auth", function(request, response) {
-	// Capture the input fields
-	let username = request.body.username;
-	let password = request.body.password;
-	// Ensure the input fields exists and are not empty
-	if (username && password) {
-		// Execute SQL query that'll select the account from the database based on the specified username and password
-		db.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			// If there is an issue with the query, output the error
-			if (error) throw error;
-			// If the account exists
-			if (results.length > 0) {
-				// Authenticate the user
-				request.session.loggedin = true;
-				request.session.username = username;
-                console.log("user connecté");
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
+app.post("/auth", function (request, response) {
+    // Capture the input fields
+    let username = request.body.username;
+    let password = request.body.password;
+    // Ensure the input fields exists and are not empty
+    if (username && password) {
+        // Execute SQL query that'll select the account from the database based on the specified username and password
+        db.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+            // If there is an issue with the query, output the error
+            if (error) throw error;
+            // If the account exists
+            if (results.length > 0) {
+                // Authenticate the user
+                request.session.loggedin = true;
+                request.session.username = username;
+                console.log("je suis connecté");
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }
+            response.end();
+        });
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
 });
+
+app.get("/mail", (req, res) => {
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+        // Generate test SMTP service account from ethereal.email
+        // Only needed if you don't have a real mail account for testing
+        let testAccount = await nodemailer.createTestAccount();
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: testAccount.user, // generated ethereal user
+                pass: testAccount.pass, // generated ethereal password
+            },
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"Alexis LB" <alexis.lebail@laposte.net>', // sender address
+            to: "alexis@lebail.eu", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: "Amélie n'a aucune utilité, je veux rentrer à la maison tout de suite", // plain text body
+            html: "<b>Hi all</b>", // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    }
+
+    main().catch(console.error);
+})
+
 
 
 app.listen(port, () => {
